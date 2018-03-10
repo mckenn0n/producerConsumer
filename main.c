48696dd33460b72e1fab955be825bbccfa835a82
McKennon McMillian <mbmcmill@go.olemiss.edu>
2018-03-09 19:23:31 -0600

    Prints need work

--- main.c
+++ main.c
@@ -1,52 +1,111 @@
 #include <stdlib.h> 
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdio.h>
 #include "buffer.h"
 
 buffer_item buffer[BUFFER_SIZE];
-
-int main(int argc, char *argv[]) {
-	/* 1. Get command line arguments argv[1],argv[2],argv[3] */
-	/* 2. Initialize buffer */
-	/* 3. Create producer thread(s) */
-	/* 4. Create consumer thread(s) */
-	/* 5. Sleep */
-	/* 6. Exit */
-}
+pthread_mutex_t mutex;
+sem_t empty;
+sem_t full;
+int timeToExit, pros, cons, i, run;
+// pthread_mutex_lock(&mutex);
+// pthread_mutex_unlock(&mutex);
 
 int insert_item(buffer_item item) {
-	/* insert item into buffer
-	return 0 if successful, otherwise
-	return -1 indicating an error condition */
+	if (i < BUFFER_SIZE){
+		buffer[i++] = item;
+	} else{
+		printf("Something went wrong while inserting an item.\nIndex is %d\n", i);
+		return -1;
+	} 
+	return 0;
 }
 
-int remove_item(buffer_item *item) {
-	/* remove an object from buffer
-	placing it in item
-	return 0 if successful, otherwise
-	return -1 indicating an error condition */
+int remove_item() {
+	if (i > 0){
+		return buffer[--i];
+	} else{
+		printf("Something went wrong while trying to remove an item.\nIndex is %d\n", i);
+		return -1;
+	}
+	return 0;
 }
 
 void *producer(void *param) {
 	buffer_item item;
-	while (true) {
-		/* sleep for a random period of time */
-		sleep(...);
-		/* generate a random number */
+	run = 1;
+	while (run) {
+		sleep(rand() % 10);
 		item = rand();
-		if (insert_item(item))
-		fprintf("report error condition");
+		sem_wait(&full);
+		pthread_mutex_lock(&mutex);
+		if (insert_item(item) == -1)
+		printf("Report error condition PRODUCER");
 		else
-		printf("producer produced %d\n",item);
+		printf("Producer produced %d\n",item);
+		pthread_mutex_unlock(&mutex);
+		sem_post(&empty);
 	}
+	pthread_exit(0);
 }
 
 void *consumer(void *param) {
 	buffer_item item;
-	while (true) {
-		/* sleep for a random period of time */
-		sleep(...);
-		if (remove_item(&item))
-		fprintf("report error condition");
+	run = 1;
+	while (run) {
+		sleep(rand() % 10);
+		sem_wait(&empty);
+		pthread_mutex_lock(&mutex);
+		item = remove_item();
+		if (item == -1)
+		printf("Report error condition CONSUMER");
 		else
-		printf("consumer consumed %d\n",item);
+		printf("Consumer consumed %d\n",item);
+		pthread_mutex_unlock(&mutex);
+		sem_post(&full);
 	}	
+	pthread_exit(0);
+}
+
+int main(int argc, char *argv[]) {
+	i = 0;
+	/* 1. Get command line arguments argv[1],argv[2],argv[3] */
+	timeToExit = atoi(argv[1]);
+	pros = atoi(argv[2]);
+	cons = atoi(argv[3]);
+	/* 2. Initialize buffer */
+	sem_init(&full, 0, BUFFER_SIZE);
+	sem_init(&empty, 0, 0);
+	pthread_mutex_init(&mutex, NULL);
+	/* 3. Create producer thread(s) */
+	pthread_t producers[pros];
+	int x;
+	for (x = 0; x < pros; x++){
+		pthread_create(&producers[x], NULL, producer, 0);
+	}
+	 // 4. Create consumer thread(s) 
+	pthread_t consumers[cons];
+	int y;
+	for (y = 0; y < cons; y++){
+		pthread_create(&consumers[y], NULL, consumer, 0);
+	}
+	/* 5. Sleep */
+	
+	sleep(timeToExit);
+	run = 0;
+	/* 6. Exit */
+
+	for (x = 0; x < pros; x++)
+		pthread_join(producers[x], NULL);
+
+	for (y = 0; y < cons; y++)
+		pthread_join(consumers[y], NULL);
+
+	pthread_mutex_destroy(&mutex);
+	sem_destroy(&full);
+	sem_destroy(&empty);
+
+	return 0;
 }
